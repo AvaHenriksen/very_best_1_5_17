201af7fba546b8611ca5dd4de2d04bb767a853fe
Raghu Betina <raghu@firstdraft.com>
2017-01-05 19:20:23 +0000

    Only userbookmark user can modify userbookmark

--- app/controllers/userbookmarks_controller.rb
+++ app/controllers/userbookmarks_controller.rb
@@ -1,4 +1,14 @@
 class UserbookmarksController < ApplicationController
+  before_action :current_user_must_be_userbookmark_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_userbookmark_user
+    userbookmark = Userbookmark.find(params[:id])
+
+    unless current_user == userbookmark.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @userbookmarks = Userbookmark.all
 
