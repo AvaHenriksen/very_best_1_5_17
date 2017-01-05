4e1636d221be843252ea6fbcf9c02402d508ac83
Raghu Betina <raghu@firstdraft.com>
2017-01-05 19:19:45 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "dishes#index"
   # Routes for the Userbookmark resource:
   # CREATE
   get "/userbookmarks/new", :controller => "userbookmarks", :action => "new"
